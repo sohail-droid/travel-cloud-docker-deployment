fea76ce650e63669e04bdbdc46b7eeaba155b641
Smd Sohail <114752507+sohail-droid@users.noreply.github.com>
2025-08-08 17:49:21 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update -y && apt-get install nginx -y
 systemctl enable nginx && systemctl start nginx
-echo "Hi all! Hope you had a great session on Dual VPC, IPv4 & IPv6 Load Balancers!" > /var/www/html/index.nginx-debian.html
+echo "Hi all! This is the NEW version deployed at $(date)" > /var/www/html/index.nginx-debian.html
